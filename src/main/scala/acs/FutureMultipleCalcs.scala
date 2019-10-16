e1de721896ca86ba029c818a45bdf4c06396620f
Alvaro del Castillo San Felix <adelcastillo@thingso2.com>
2019-10-16 05:21:15 +0200

    Add more sample logic about using Futures

--- src/main/scala/acs/FutureMultipleCalcs.scala
+++ src/main/scala/acs/FutureMultipleCalcs.scala
@@ -1,13 +1,16 @@
 package acs
 
+import scala.concurrent.Await
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 import scala.util.Random
 
 // Sample from: https://alvinalexander.com/scala/concurrency-with-scala-futures-tutorials-examples
 
 object Cloud {
+
   def runAlgorithm(i: Int): Future[Int] = Future {
     Thread.sleep(Random.nextInt(500))
     val result = i + 10
@@ -17,6 +20,7 @@ object Cloud {
 }
 
 object FutureMultipleCalcs extends App {
+
   println("starting futures")
   val result1 = Cloud.runAlgorithm(10)
   val result2 = Cloud.runAlgorithm(20)
@@ -36,5 +40,48 @@ object FutureMultipleCalcs extends App {
   }
 
   println("before sleep at the end")
-  Thread.sleep(2000)  // important: keep the jvm alive
+  Thread.sleep(2000) // important: keep the jvm alive
+}
+
+object FutureLife extends App {
+
+  // A future execution depends on other future result
+
+  class Bar(name: String) {
+
+    def isOpen(): Future[Boolean] = Future {
+      Thread.sleep(Random.nextInt(500))
+      Random.nextBoolean()
+    }
+
+    def hasBeer(): Future[Boolean] = Future {
+      Thread.sleep(Random.nextInt(500))
+      Random.nextBoolean()
+    }
+  }
+
+  def drinkBeerInFuture(bar: Bar): Future[Future[Future[Boolean]]] = Future {
+    bar.isOpen().map { open =>
+      open match {
+        case true => bar.hasBeer()
+        case _ => throw new Exception("Bar is closed")
+      }
+    }
+  }
+
+  def drinkBeerNow(bar: Bar) = {
+    val beer = Await.result(
+    bar.isOpen().flatMap { open =>
+      open match {
+        case true => bar.hasBeer()
+        case _ => Future(false)
+      }
+    }, 10 seconds)
+
+    if (beer) println("Drinking beer")
+    else println("Can not drink beer! :(")
+  }
+
+  drinkBeerNow(new Bar("Luz"))
+
 }
