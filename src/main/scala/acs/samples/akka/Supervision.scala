bff105870a0321a39108e8efad853376f843985b
Alvaro del Castillo San Felix <adelcastillo@thingso2.com>
2019-11-07 09:50:04 +0100

    Add empty sample file for future Supervision samples

---
+++ src/main/scala/acs/samples/akka/Supervision.scala
@@ -0,0 +1,10 @@
+package acs.samples.akka
+
+
+/*
+  https://github.com/akka/akka-samples/tree/2.5/akka-sample-supervision-java
+  Congratulations! You have just created your first fault-resilient Akka application, nice job!
+ */
+object Supervision {
+
+}
